e6e6201c8db50f97173590a922ef0f479adb227e
Rafagan Abreu <rafagan@guizion.com>
2018-09-28 19:07:55 -0300

    Tela de login

--- app/src/main/java/schedule/tutorial/rafagan/androidschedule/MainActivity.kt
+++ app/src/main/java/schedule/tutorial/rafagan/androidschedule/MainActivity.kt
@@ -1,5 +1,6 @@
 package schedule.tutorial.rafagan.androidschedule
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,7 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        this.navigateToLogin()
+    }
 
-        // Teste
+    fun navigateToLogin() {
+        val myIntent = Intent(this, LoginActivity::class.java)
+        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(myIntent)
     }
 }
